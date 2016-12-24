ac83869e9dd73e93db36102b0292e4a6ca43c905
Krystal Campioni <krystal.designer@gmail.com>
2016-12-23 23:55:30 -0200

    setup sourcemaps

--- libs/parts.js
+++ libs/parts.js
@@ -24,7 +24,7 @@ exports.setupCSS = function(paths) {
       loaders: [
         {
           test: /\.css$/,
-          loaders: ['style', 'css'],
+          loaders: ['style', 'css?sourceMap'],
           include: paths
         }
       ]
